1de4165d073bc785baf2b3f31bf766d10e007177
ᴄʀᴀᴢʏXʏxx <davidjnior2000@gmail.com>
2025-04-02 12:49:04 +0100

    Delete story.js

--- story.js
+++
@@ -1,9 +0,0 @@
-
-// JavaScript to animate the image based on scroll
-window.addEventListener("scroll", () => {
-    const image = document.querySelector('.logo img');
-    const scrollPosition = window.scrollY;
-
-    // Apply a transformation to the logo based on the scroll position
-    image.style.transform = `rotate(scrollPosition * 0.1deg) scale({1 + scrollPosition * 0.001})`;
-});
